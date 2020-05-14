30f1728cdbe7248d487b20647a631c57b4af98b9
Dan <dan.z.haas@gmail.com>
2020-05-14 16:09:56 -0400

    Updates

---
+++ precache-manifest.5c8599786a68aa6d96c6f6bf924774b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f56cbd5b0f4c83220833531e4e4970b",
+    "url": "/barkr-react/index.html"
+  },
+  {
+    "revision": "11782ccb4a6e3b5fe82d",
+    "url": "/barkr-react/static/css/2.8d59620d.chunk.css"
+  },
+  {
+    "revision": "ed3811cfc10a3873c925",
+    "url": "/barkr-react/static/css/main.bfce6dbc.chunk.css"
+  },
+  {
+    "revision": "11782ccb4a6e3b5fe82d",
+    "url": "/barkr-react/static/js/2.6dca96fc.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/barkr-react/static/js/2.6dca96fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed3811cfc10a3873c925",
+    "url": "/barkr-react/static/js/main.7f24c487.chunk.js"
+  },
+  {
+    "revision": "3f4e12f47bbe6abc97f4",
+    "url": "/barkr-react/static/js/runtime-main.5c60889a.js"
+  }
+]);
